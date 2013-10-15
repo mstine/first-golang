26a7b17afb6396e45e1ad41ebf5947032176b5c5
Matt Stine <mstine@gopivotal.com>
2013-10-15 16:41:05 -0500

    square root exercise

--- main/main.go
+++ main/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+  "os"
+  "fmt"
+  "math"
+  "strconv"
+)
+
+const Delta = 0.00000001
+
+func Sqrt(x float64) (float64, int) {
+  currentGuess := 1.0
+  for i := 1; ;i++ {
+    nextGuess := guess(x, currentGuess)
+    if goodEnough(currentGuess, nextGuess) {
+      return currentGuess, i
+    } else {
+      currentGuess = nextGuess 
+    }
+  }
+}
+
+func guess(arg float64, current float64) float64 {
+    return current - ((math.Pow(current,2) - arg) / (2 * current)) 
+}
+
+func goodEnough(last float64, current float64) bool {
+  return math.Abs(last - current) < Delta
+}
 
 func main() {
-  fmt.Println("Hello, World!")
+  arg, err := strconv.ParseFloat(os.Args[1],64)
+  if (err == nil) {
+    result, iterations := Sqrt(arg)
+    fmt.Printf("Iterations: %v\n", iterations)
+    fmt.Printf("My Sqrt   = %v\n", result)
+    fmt.Printf("math.Sqrt = %v\n", math.Sqrt(arg))
+  } else {
+    fmt.Printf("Error calculating square root: %v\n",err)
+  }
 }
